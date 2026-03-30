4ec4412f53c3f927bdaadb1f53fc056d56d5b2b3
cblakely97 <coleman.blakely@gmail.com>
2026-03-30 13:12:11 -0700

    Fix minor bugs in tides test case
       - Adds remap topography step to tides case to update cull_mesh
         behavior

--- compass/ocean/tests/tides/mesh/__init__.py
+++ compass/ocean/tests/tides/mesh/__init__.py
@@ -1,7 +1,8 @@
-from compass.testcase import TestCase
 from compass.mesh.spherical import IcosahedralMeshStep
-from compass.ocean.tests.tides.configure import configure_tides
 from compass.ocean.mesh.cull import CullMeshStep
+from compass.ocean.mesh.remap_topography import RemapTopography
+from compass.ocean.tests.tides.configure import configure_tides
+from compass.testcase import TestCase
 
 
 class Mesh(TestCase):
@@ -38,9 +39,14 @@ class Mesh(TestCase):
 
         self.add_step(base_mesh_step)
 
+        remap_step = RemapTopography(test_case=self,
+                                     base_mesh_step=base_mesh_step,
+                                     mesh_name=mesh_name)
+        self.add_step(remap_step)
+
         self.add_step(CullMeshStep(
             test_case=self, base_mesh_step=base_mesh_step,
-            with_ice_shelf_cavities=True))
+            with_ice_shelf_cavities=True, unsmoothed_topo=remap_step))
 
     def configure(self):
         """
